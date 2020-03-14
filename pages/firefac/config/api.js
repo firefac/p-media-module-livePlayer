44ff0fcca22769700c193693cc9a8badfe392e35
simon zhong <zhong.yu@xiangwushuo.com>
2020-03-15 00:32:19 +0800

    init

--- pages/firefac/config/api.js
+++ pages/firefac/config/api.js
@@ -1,8 +1,8 @@
 // 以下是业务服务器API地址
 // 云平台测试时使用
-//var WxApiRoot = 'https://test-0j2wkomi3jpe.firefac.cn/openapi/'
+var WxApiRoot = 'https://test-0j2wkomi3jpe.firefac.cn/openapi/'
 // 云平台上线时使用
-var WxApiRoot = 'https://ell968bhpgj5.firefac.cn/openapi/';
+//var WxApiRoot = 'https://ell968bhpgj5.firefac.cn/openapi/';
 
 module.exports = {
   LiveRoomList: WxApiRoot + 'live/list', //直播房间列表
